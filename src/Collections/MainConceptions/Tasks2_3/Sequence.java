3fbef7f2bf35c76d7058f13c9dd14a77884a54f9
atyma <atymkiv99@gmail.com>
2018-06-14 16:23:50 +0300

    Added possibility to add any quantity of objects to container

---
+++ src/Collections/MainConceptions/Tasks2_3/Sequence.java
@@ -0,0 +1,42 @@
+package Collections.MainConceptions.Tasks2_3;
+
+interface Selector {
+    boolean end();
+    Object current();
+    void next();
+}
+
+public class Sequence {
+    private Object[] items;
+    private int next = 0;
+    public Sequence(int size) {
+        items = new Object[size];
+    }
+    public void add(Object x) {
+        if(next < items.length)
+            items[next++] = x;
+    }
+    private class SequenceSelector implements Selector {
+        private int i = 0;
+        @Override
+        public boolean end() { return i == items.length; }
+        @Override
+        public Object current() { return items[i]; }
+        @Override
+        public void next() { if(i < items.length) i++; }
+    }
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+    public static void main(String[] args) {
+        int size = Integer.parseInt(args[0]);
+        Sequence sequence = new Sequence(size);
+        for(int i = 0; i < size; i++)
+            sequence.add(Integer.toString(i));
+        Selector selector = sequence.selector();
+        while(!selector.end()) {
+            System.out.print(selector.current() + " ");
+            selector.next();
+        }
+    }
+}
