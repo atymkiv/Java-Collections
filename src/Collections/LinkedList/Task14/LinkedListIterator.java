2e771336774adbc66aee06190332a2fd3d21ddee
atyma <atymkiv99@gmail.com>
2018-06-18 21:47:35 +0300

    created empty linkedList then filled this list by inserting in the middle of this list new elements by ListIterator

--- src/Collections/LinkedList/Task14/LinkedListIterator.java
+++ src/Collections/LinkedList/Task14/LinkedListIterator.java
@@ -7,18 +7,14 @@ public class LinkedListIterator {
     public static void main(String[]args){
         LinkedList<Integer> linkedList = new LinkedList<>();
         ListIterator<Integer> listIterator = linkedList.listIterator();
-        for (int i = 0; i < 10; i++) {
-          listIterator.add(0);
-        }
-        listIterator = linkedList.listIterator(4);
-        listIterator.next();
-        for (int i = 1; i <= 10; i++) {
-            listIterator.add(i);
+        int index=0;
+        for (int i = 0; i < 20; i++) {
+            index= linkedList.size()/2;
+            if (index>0){
+                listIterator = linkedList.listIterator(index);
+            }
+          listIterator.add(i);
             System.out.println(linkedList + " Розмір = " + linkedList.size());
-
-
         }
-
-
     }
 }
